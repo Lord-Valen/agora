b4f0e3405af312aa0f3f3ed6bdc878d9b9180b46
Lord-Valen <lord_valen@protonmail.com>
2022-04-29 16:24:00 -0400

    Refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,32 @@ const fs = require("fs");
 const { token } = require("./config.json");
 const { Client, Intents } = require("discord.js");
 
+// Start client
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS],
-  partials: ["MESSAGE", "REACTION"],
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
 });
 
+// Login
 client.on("ready", () => {
   console.log("All set!");
 });
-
 client.login(token);
 
-const eventFiles = fs
-  .readdirSync("./events")
-  .filter((file: string) => file.endsWith(".js"));
+// Event Handler
+fs.readdir(__dirname + "/events", (err: string, files: string[]) => {
+  if (err) return console.error(err);
+  files.forEach((file: string) => {
+    if (file.endsWith(".js") == false) return;
+    const event = require(`./events/${file}`);
+    console.log(`Requiring ${file}`);
+    if (event.once) {
+      client.once(event.name, (...args: any[]) => event.execute(...args));
+    } else {
+      console.log(`Listening for ${event.name}`);
+      client.on(event.name, (...args: any[]) => event.execute(...args));
+    }
+  });
+});
 
-for (const file of eventFiles) {
-  const event = require(`./events/${file}`);
-
-  if (event.once) {
-    client.once(event.name, (...args: any[]) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args: any[]) => event.execute(...args));
-  }
-}
+module.exports = client;
