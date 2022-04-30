bbe94f9ad8dcbd7d3c6580292b138f87c73b3e63
Lord-Valen <lord_valen@protonmail.com>
2022-04-29 22:26:36 -0400

    Update

--- flake.nix
+++ flake.nix
@@ -1,13 +1,17 @@
 {
   description = "A starboarding Discord bot";
 
-  inputs = { fu.url = "github:numtide/flake-utils"; };
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    fu.url = "github:numtide/flake-utils";
+  };
 
   outputs = { self, nixpkgs, fu }:
     fu.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
-        devShell =
-          pkgs.mkShell { packages = with pkgs; [ nodejs nodePackages.npm ]; };
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [ nodejs nodePackages.npm nixfmt ];
+        };
       });
 }
